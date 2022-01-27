cc53c0f58b1e842292f7a221b574060984a7bbfc
Gaia Guinouard-Balconi-Weida <gaiagoessocial@gmail.com>
2022-01-27 15:00:29 -0500

    bcs

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);   //Now all of the routes defined in comment-routes.js will have a /comments prefix. This is how you can easily scale APIs; just add a new endpoint!
 
 module.exports = router;
